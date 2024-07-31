eb367f482959662613dbd9c5e93b1e0b839b5903
dalepantano <dalepantano54@gmail.com>
2024-07-31 15:42:38 +0800

    Initial commit

---
+++ app/src/main/java/com/example/stellaron/blade.kt
@@ -0,0 +1,27 @@
+package com.example.stellaron
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+class blade: AppCompatActivity()  {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_blade)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val charactersButton = findViewById<Button>(R.id.button2)
+        charactersButton.setOnClickListener {
+            // Create an Intent to start MainActivity
+            val intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent)
+        }
+    }
+}
